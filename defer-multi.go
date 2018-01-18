64ba84b96d3736cca031f610d56a7749ecc91de2
Ryo Takashima <r.takashima@miniascape.co.jp>
2018-01-18 15:14:29 +0900

    defer-multi

---
+++ defer-multi.go
@@ -0,0 +1,15 @@
+// defer へ渡した関数が複数ある場合、その呼び出しはスタック( stack )されます。
+// 呼び出し元の関数がreturnするとき、 defer へ渡した関数は LIFO(last-in-first-out) の順番で実行されます。
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
+}
