05944756f344636fa165d53cfa327fdf8eec6661
Ryo Takashima <r.takashima@miniascape.co.jp>
2018-01-18 15:11:06 +0900

    defer

---
+++ defer.go
@@ -0,0 +1,10 @@
+// defer ステートメントは、 defer へ渡した関数の実行を、呼び出し元の関数の終わり(returnする)まで遅延させるものです。
+// defer へ渡した関数の引数は、すぐに評価されますが、その関数自体は呼び出し元の関数がreturnするまで実行されません。
+package main
+
+import "fmt"
+
+func main() {
+	defer fmt.Println("world")
+	fmt.Println("hello")
+}
