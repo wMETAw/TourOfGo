6f37e3abc3be9f528cee0a53ee7e877c013cb4a3
Ryo Takashima <r.takashima@miniascape.co.jp>
2018-01-19 14:05:34 +0900

    nil interface value

---
+++ nil-interface-values.go
@@ -0,0 +1,21 @@
+// nil インターフェースの値は、値も具体的な型も保持しません。
+//
+// 呼び出す 具体的なメソッドを示す型がインターフェースのタプル内に存在しないため、
+// nil インターフェースのメソッドを呼び出すと、ランタイムエラーになります。
+package main
+
+import "fmt"
+
+type I interface {
+	M()
+}
+
+func main() {
+	var i I
+	describe(i)
+	i.M()
+}
+
+func describe(i I) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
